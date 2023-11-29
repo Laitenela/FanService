a85f04d7a7fe943d8203f1aa8d66e395f0d17781
Laitenela <vladulanov-12@mail.ru>
2023-11-29 23:24:21 +0300

    Initial Commit

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,16 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-      "^.+\\.tsx?$": "ts-jest" 
-  // process `*.tsx` files with `ts-jest`
+    "^.+\\.tsx?$": "ts-jest"
+    // process `*.tsx` files with `ts-jest`
   },
   rootDir: './',
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ["^.*\\.spec\\.[jt]s?$"],
+  testPathIgnorePatterns: ["^.*\\.spec\\.[jt]s?$"],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-    '<rootDir>/src/shared/storybook/__mocks__/fileMock.ts',
-  '\\.(css|less|scss)$': '<rootDir>/src/shared/storybook/__mocks__/styleMock.ts',
+      '<rootDir>/src/shared/storybook/__mocks__/fileMock.ts',
+    '\\.(css|less|scss)$': '<rootDir>/src/shared/storybook/__mocks__/styleMock.ts',
   },
 }
